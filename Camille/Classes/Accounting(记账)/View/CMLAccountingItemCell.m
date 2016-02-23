26b61be825560942dfce26fae2b63ee220269d99
ShayneYeorg <mercury10@qq.com>
2016-02-23 14:44:05 +0800

    建立记账一级科目和二级科目表

--- Camille/Classes/Accounting(记账)/View/CMLAccountingItemCell.m
+++ Camille/Classes/Accounting(记账)/View/CMLAccountingItemCell.m
@@ -8,14 +8,19 @@
 
 #import "CMLAccountingItemCell.h"
 
-@interface CMLAccountingItemCell ()
+@interface CMLAccountingItemCell () <UITableViewDelegate, UITableViewDataSource>
 
 @property (weak, nonatomic) IBOutlet UIView *topView; //上部区域
 @property (weak, nonatomic) IBOutlet UIView *bottomView; //选择项目的区域
+@property (weak, nonatomic) IBOutlet UITableView *leftTableView;
+@property (weak, nonatomic) IBOutlet UITableView *rightTableView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *leftTableViewWidthContraint;//左菜单的宽度约束
 
 @end
 
 @implementation CMLAccountingItemCell
+
+#pragma mark - Public
 
 + (instancetype)loadFromNib {
     CMLAccountingItemCell *cell = [[[NSBundle mainBundle]loadNibNamed:@"CMLAccountingItemCell" owner:self options:nil] firstObject];
@@ -41,6 +46,8 @@
     }
 }
 
+#pragma mark - Private
+
 - (IBAction)expandTap:(id)sender {
     if ([self.delegate respondsToSelector:@selector(accountingItemCellDidTapExpandArea:)]) {
         [self.delegate accountingItemCellDidTapExpandArea:self];
@@ -48,13 +55,30 @@
 }
 
 - (void)awakeFromNib {
-    // Initialization code
+    self.leftTableViewWidthContraint.constant = kContent_Width * 0.35;
+    
+    self.leftTableView.delegate = self;
+    self.leftTableView.dataSource = self;
+    self.leftTableView.tableFooterView = [UIView new];
+    
+    self.rightTableView.delegate = self;
+    self.rightTableView.dataSource = self;
+    self.rightTableView.tableFooterView = [UIView new];
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
+#pragma mark - TableViewDataSource
 
-    // Configure the view for the selected state
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc]init];
+    return cell;
 }
 
 @end
+
+
+
+

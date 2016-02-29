6e5617f4d277418938470b9c2a6ed3657a717637
ShayneYeorg <mercury10@qq.com>
2016-02-29 16:50:47 +0800

    增加新增一级记账科目的相关方法

--- Camille/Classes/Public(公共)/Tools/CMLCoreDataAccess.m
+++ Camille/Classes/Public(公共)/Tools/CMLCoreDataAccess.m
@@ -85,6 +85,87 @@
     });
 }
 
+//新增一级记账科目
++ (void)addItemCategory:(NSString *)ItemCategoryName callBack:(void(^)(CMLResponse *response))callBack {
+    CMLLog(@"新增一级记账科目所在的线程是：%@", [NSThread currentThread]);
+    
+    //Response
+    CMLResponse *cmlResponse = [[CMLResponse alloc]init];
+    
+    //分配新ID
+    NSString *newID = [CMLCoreDataAccess getANewItemCategoryID];
+    if (newID == nil) {
+        cmlResponse.code = RESPONSE_CODE_FAILD;
+        cmlResponse.desc = @"分配新的一级科目ID出错";
+        cmlResponse.responseDic = nil;
+        callBack(cmlResponse);
+        
+    } else {
+        //Entity
+        CMLItemCategory *itemCategory = [NSEntityDescription insertNewObjectForEntityForName:@"CMLItemCategory" inManagedObjectContext:kManagedObjectContext];
+        itemCategory.categoryName = ItemCategoryName;
+        itemCategory.categoryID = newID;
+        itemCategory.nextCategoryID = nil;
+        
+        //保存
+        NSError *error = nil;
+        if ([kManagedObjectContext save:&error]) {
+            if (error) CMLLog(@"新增时发生错误:%@,%@",error,[error userInfo]);
+            cmlResponse.code = RESPONSE_CODE_FAILD;
+            cmlResponse.desc = @"保存一级科目出错";
+            cmlResponse.responseDic = nil;
+            
+        } else {
+            cmlResponse.code = RESPONSE_CODE_SUCCEED;
+            cmlResponse.desc = @"保存一级科目成功";
+            cmlResponse.responseDic = [NSDictionary dictionaryWithObjectsAndKeys:newID, @"itemCategoryID", nil];
+        }
+        
+        //回调
+        callBack(cmlResponse);
+    }
+}
+
+//新分配一个一级记账科目的ID
++ (NSString *)getANewItemCategoryID {
+    //先获取最大ID，再加1
+    CMLLog(@"新分配一个一级记账科目的ID所在的线程是：%@", [NSThread currentThread]);
+    
+    //request和entity
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"CMLItemCategory" inManagedObjectContext:kManagedObjectContext];
+    [request setEntity:entity];
+    
+    //设置排序规则
+    NSSortDescriptor *sort = [[NSSortDescriptor alloc] initWithKey:@"createTime" ascending:NO comparator:^(CMLItemCategory *obj1, CMLItemCategory *obj2) {
+        NSInteger idNum1 = [obj1.categoryID integerValue];
+        NSInteger idNum2 = [obj2.categoryID integerValue];
+        return [@(idNum1) compare:@(idNum2)];
+    }];
+    NSArray * sortDescriptors = @[sort];
+    [request setSortDescriptors:sortDescriptors];
+    
+    //设置数据条数
+    [request setFetchLimit:1];
+    [request setFetchOffset:0];
+    
+    //查询
+    NSError *error = nil;
+    NSMutableArray *itemCategories = [[kManagedObjectContext executeFetchRequest:request error:&error] mutableCopy];
+    if (itemCategories.count) {
+        CMLItemCategory *ic = itemCategories[0];
+        NSInteger newID = ic.categoryID.integerValue + 1;
+        CMLLog(@"新分配的一级科目ID是：%zd", newID);
+        return [NSString stringWithFormat:@"%zd", newID];
+        
+    } else {
+        //要么是查询过程中出错
+        //要么是第一个一级科目（这是不可能的）
+        CMLLog(@"查询一级科目最大ID时发生错误:%@,%@",error,[error userInfo]);
+        return nil;
+    }
+}
+
 //新增数据
 + (void)addAccountingWithItem:(NSString *)itemID amount:(NSNumber *)amount happneTime:(NSDate *)happenTime callBack:(void(^)(CMLResponse *response))callBack {
     //Entity

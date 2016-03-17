af3d2934705614d159e003f886f209b816753c03
ShayneYeorg <mercury10@qq.com>
2016-03-17 16:58:31 +0800

    校验处理

--- Camille/Classes/Accounting(记账)/View/CMLAccountingAmountCell.m
+++ Camille/Classes/Accounting(记账)/View/CMLAccountingAmountCell.m
@@ -43,6 +43,24 @@
 - (void)textFieldDidEndEditing:(UITextField *)textField {
     if (!self.amountTextField.text.length) {
         self.amountTextField.text = @"金额";
+        
+    } else {
+        //是否有小数点？
+        NSString *searchText = textField.text;
+        NSError *pointError = NULL;
+        NSRegularExpression *pointRegex = [NSRegularExpression regularExpressionWithPattern:@"[.]" options:NSRegularExpressionCaseInsensitive error:&pointError];
+        NSTextCheckingResult *result = [pointRegex firstMatchInString:searchText options:0 range:NSMakeRange(0, [searchText length])];
+        if (result) {
+            //有小数点
+            //看看是否需要补位数
+            NSError *afterPointError = NULL;
+            NSRegularExpression *pointRegex = [NSRegularExpression regularExpressionWithPattern:@"[.][0-9]{2,}" options:NSRegularExpressionCaseInsensitive error:&afterPointError];
+            NSTextCheckingResult *result = [pointRegex firstMatchInString:searchText options:0 range:NSMakeRange(0, [searchText length])];
+            
+        } else {
+            //无小数点，补上小数点和后两位
+            textField.text = [NSString stringWithFormat:@"%@.00", textField.text];
+        }
     }
 }
 
